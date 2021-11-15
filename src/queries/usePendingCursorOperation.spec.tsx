67aa6162a27b2f67b67363981f5832c772dbb16e
Joaquín Guillén <joaquin@develop17.com>
2021-11-15 09:40:56 +0100

    Adds a reset function to the pending cursor operation.

--- src/queries/usePendingCursorOperation.spec.tsx
+++ src/queries/usePendingCursorOperation.spec.tsx
@@ -174,4 +174,77 @@ describe('usePendingCursorOperation tests', function () {
       ).to.eq(true)
     }
   })
+
+  it('should correctly reset the current live query and create a new one when the reset function is called.', async () => {
+    const testConfiguration = testIdentity()
+    const initOptions = {
+      webAssemblyModule: '/base/node_modules/@dittolive/ditto/web/ditto.wasm',
+    }
+
+    const TestComponent: React.FC = () => {
+      const { ditto, insert } = useMutations<unknown>({
+        path: testConfiguration.path,
+        collection: 'foo',
+      })
+
+      useEffect(() => {
+        if (ditto) {
+          insert({ value: { document: 1 } })
+          insert({ value: { document: 2 } })
+          insert({ value: { document: 3 } })
+          insert({ value: { document: 4 } })
+          insert({ value: { document: 5 } })
+        }
+      }, [ditto])
+
+      return <></>
+    }
+
+    const wrapper = ({ children }: { children: ReactNode }) => (
+      <DittoProvider
+        setup={() => {
+          const ditto = new Ditto(
+            testConfiguration.identity,
+            testConfiguration.path,
+          )
+          return ditto
+        }}
+        initOptions={initOptions}
+      >
+        {() => {
+          return (
+            <>
+              <TestComponent />
+              {children}
+            </>
+          )
+        }}
+      </DittoProvider>
+    )
+
+    const params: LiveQueryParams = {
+      path: testConfiguration.path,
+      collection: 'foo',
+      query: 'document > 3',
+    }
+    const { result, waitFor } = renderHook(
+      () => usePendingCursorOperation(params),
+      {
+        wrapper,
+      },
+    )
+    await waitFor(() => !!result.current.documents?.length, { timeout: 5000 })
+
+    expect(result.current.documents.length).to.eq(2)
+
+    const liveQueryBeforeReset = result.current.liveQuery
+
+    result.current.reset()
+
+    await waitFor(() => result.current.liveQuery !== liveQueryBeforeReset, {
+      timeout: 5000,
+    })
+
+    expect(result.current.documents.length).to.eq(2)
+  })
 })
