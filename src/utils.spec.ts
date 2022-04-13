233db0885d6598d576c96d9f87fed928eba973c3
Jonathan Reyes <j@jreyes.org>
2022-04-13 17:43:14 -0500

    Fix lint error

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,8 +1,5 @@
 /** Helper function used to wait for events to sink to the DOM before assertions can be made. */
-export const waitFor = (
-  cb: () => boolean,
-  waitMs: number = 300,
-): Promise<void> => {
+export const waitFor = (cb: () => boolean, waitMs = 300): Promise<void> => {
   return new Promise((resolve) => {
     const interval = setInterval(() => {
       const result = cb()
