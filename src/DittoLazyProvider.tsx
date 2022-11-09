eadb6e87da0544c85cddf48a8a638bcbfec3ab0e
Jonathan Reyes <j@jreyes.org>
2022-11-09 12:27:32 -0500

    chore: reformat code after upgrading `prettier`

--- src/DittoLazyProvider.tsx
+++ src/DittoLazyProvider.tsx
@@ -30,107 +30,108 @@ export interface DittoLazyProviderProps {
  * @param props
  * @returns A function that needs to return a React.Element
  */
-export const DittoLazyProvider: React.FunctionComponent<DittoLazyProviderProps> =
-  ({ initOptions, setup, render, children }) => {
-    const [dittoHash, setDittoHash] = useState<DittoHash>({})
-    const createdPaths = useRef([])
-    const [providerState, setProviderState] = useState<ProviderState>({
-      loading: true,
-      error: undefined,
-    })
-    const hasMountEffectStarted = useRef(false)
+export const DittoLazyProvider: React.FunctionComponent<
+  DittoLazyProviderProps
+> = ({ initOptions, setup, render, children }) => {
+  const [dittoHash, setDittoHash] = useState<DittoHash>({})
+  const createdPaths = useRef([])
+  const [providerState, setProviderState] = useState<ProviderState>({
+    loading: true,
+    error: undefined,
+  })
+  const hasMountEffectStarted = useRef(false)
 
-    useEffect(() => {
-      if (hasMountEffectStarted.current) {
-        return
-      }
-
-      ;(async function () {
-        hasMountEffectStarted.current = true
-
-        try {
-          await init(initOptions)
-
-          setProviderState({
-            error: undefined,
-            loading: false,
-          })
-        } catch (err) {
-          setProviderState({
-            error: err,
-            loading: false,
-          })
-        }
-      })()
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    const renderFunction: RenderFunction | undefined = render || children
-
-    let child: ReactNode = <></>
-    if (renderFunction) {
-      child = renderFunction(providerState)
+  useEffect(() => {
+    if (hasMountEffectStarted.current) {
+      return
     }
 
-    const handleLoadInstance = async (appPath: string) => {
-      if (createdPaths.current.includes(appPath)) {
-        return dittoHash[appPath] || null
-      } else {
-        createdPaths.current.push(appPath)
-        /** The app path is initialized to null while loading to avoid parallel initializations
-         * of the same instance.
-         */
-        setDittoHash((currentHash) => ({
-          ...currentHash,
-          [appPath]: null,
-        }))
+    ;(async function () {
+      hasMountEffectStarted.current = true
 
-        const dittoInstance = await setup(appPath)
+      try {
+        await init(initOptions)
 
-        /** The setup function may choose to skip instance creation and return null, in
-         * which case we don't cache the returned value.
-         */
-        if (dittoInstance) {
-          setDittoHash((currentHash) => ({
-            ...currentHash,
-            [appPath]: dittoInstance,
-          }))
-        }
-
-        return dittoInstance
+        setProviderState({
+          error: undefined,
+          loading: false,
+        })
+      } catch (err) {
+        setProviderState({
+          error: err,
+          loading: false,
+        })
       }
-    }
+    })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-    const registerDitto: RegisterDitto = (ditto) => {
-      if (ditto.path in dittoHash) {
-        throw new Error(
-          'The instance path is already being used by a Ditto instance.',
-        )
-      }
+  const renderFunction: RenderFunction | undefined = render || children
 
+  let child: ReactNode = <></>
+  if (renderFunction) {
+    child = renderFunction(providerState)
+  }
+
+  const handleLoadInstance = async (appPath: string) => {
+    if (createdPaths.current.includes(appPath)) {
+      return dittoHash[appPath] || null
+    } else {
+      createdPaths.current.push(appPath)
+      /** The app path is initialized to null while loading to avoid parallel initializations
+       * of the same instance.
+       */
       setDittoHash((currentHash) => ({
         ...currentHash,
-        [ditto.path]: ditto,
+        [appPath]: null,
       }))
-    }
 
-    const unregisterDitto: UnregisterDitto = (path) => {
-      const hash = { ...dittoHash }
-      delete hash[path]
-      setDittoHash(hash)
-    }
+      const dittoInstance = await setup(appPath)
 
-    return (
-      <DittoContext.Provider
-        value={{
-          dittoHash: dittoHash,
-          registerDitto,
-          unregisterDitto,
-          load: handleLoadInstance,
-          isLazy: true,
-        }}
-      >
-        {child}
-      </DittoContext.Provider>
-    )
+      /** The setup function may choose to skip instance creation and return null, in
+       * which case we don't cache the returned value.
+       */
+      if (dittoInstance) {
+        setDittoHash((currentHash) => ({
+          ...currentHash,
+          [appPath]: dittoInstance,
+        }))
+      }
+
+      return dittoInstance
+    }
   }
+
+  const registerDitto: RegisterDitto = (ditto) => {
+    if (ditto.path in dittoHash) {
+      throw new Error(
+        'The instance path is already being used by a Ditto instance.',
+      )
+    }
+
+    setDittoHash((currentHash) => ({
+      ...currentHash,
+      [ditto.path]: ditto,
+    }))
+  }
+
+  const unregisterDitto: UnregisterDitto = (path) => {
+    const hash = { ...dittoHash }
+    delete hash[path]
+    setDittoHash(hash)
+  }
+
+  return (
+    <DittoContext.Provider
+      value={{
+        dittoHash: dittoHash,
+        registerDitto,
+        unregisterDitto,
+        load: handleLoadInstance,
+        isLazy: true,
+      }}
+    >
+      {child}
+    </DittoContext.Provider>
+  )
+}
