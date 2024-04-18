c4eab2a87905e310fe02028f8db59e36198e7bd6
Vincent Ahrend <vincent.ahrend@ditto.live>
2024-04-18 13:41:55 +0200

    Add `TextEncoder` polyfill to example

--- examples/create-react-app-typescript-example/src/setupTests.ts
+++ examples/create-react-app-typescript-example/src/setupTests.ts
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+
+// JSDom, which is used by @testing-library/react, doesn't support TextEncoder and TextDecoder,
+// even though they have broad support in modern browsers. This polyfill adds them to the global
+// object so that they can be used by @dittolive/ditto.
+// cf. https://github.com/jsdom/jsdom/issues/2524
+import { TextEncoder, TextDecoder } from 'util'
+global.TextEncoder = TextEncoder
+// @ts-ignore
+global.TextDecoder = TextDecoder
