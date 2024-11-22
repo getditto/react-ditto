906b4a8239e31e8fabb25896f0ffb1b444eb091b
Aaron Leopold <aaronleopold1221@gmail.com>
2024-11-22 09:22:18 -0700

    give example dev server static port

--- examples/vite-typescript-example/vite.config.ts
+++ examples/vite-typescript-example/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['@dittolive/react-ditto'],
+  server: {
+    port: 3000,
   },
 })
